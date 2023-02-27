946c804959fae43f3533dcea06a57c37a8fdbf09
Presti <dxssucuk@hotmail.com>
2023-02-27 14:46:45 +0100

    More stuff.

--- src/main/java/de/presti/ree6/sql/entities/ScheduledMessage.java
+++ src/main/java/de/presti/ree6/sql/entities/ScheduledMessage.java
@@ -2,11 +2,21 @@ package de.presti.ree6.sql.entities;
 
 import de.presti.ree6.sql.entities.webhook.WebhookScheduledMessage;
 import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
 
 /**
  * This class is used to represent a Scheduled Message.
  */
 @Entity
+@Getter
+@Setter
 @Table(name = "ScheduledMessage")
 public class ScheduledMessage {
 
@@ -23,7 +33,18 @@ public class ScheduledMessage {
     @Column(name = "message")
     public String message;
 
-    // TODO:: add options for example every hour, day, month, year and specific date.
+    // TODO:: add options for example hour, day, month, year and specific date.
+
+    @Column(name = "typ")
+    int scheduleTyp;
+
+    @Column(name = "delay")
+    int delayAmount;
+
+    @Column(name = "repeat")
+    boolean repeated;
+
+    LocalDateTime scheduleDate;
 
     /**
      * The related ScheduledMessage Webhook.
@@ -31,4 +52,20 @@ public class ScheduledMessage {
     @ManyToOne(optional = false)
     @JoinColumn(name="webhook_id", nullable=false, updatable=false)
     private WebhookScheduledMessage scheduledMessageWebhook;
+
+    /**
+     * Last updated time.
+     */
+    @UpdateTimestamp
+    @Setter(AccessLevel.PRIVATE)
+    @Temporal(TemporalType.TIMESTAMP)
+    Timestamp lastUpdated;
+
+    /**
+     * Created time.
+     */
+    @CreationTimestamp
+    @Setter(AccessLevel.PRIVATE)
+    @Temporal(TemporalType.TIMESTAMP)
+    Timestamp created;
 }
