1ab81518a396a2a9276b04b25453c13107c183e3
Presti <dxssucuk@hotmail.com>
2023-03-11 22:55:47 +0100

    Adding all args.

--- src/main/java/de/presti/ree6/sql/entities/webhook/WebhookScheduledMessage.java
+++ src/main/java/de/presti/ree6/sql/entities/webhook/WebhookScheduledMessage.java
@@ -4,11 +4,13 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 /**
  * SQL Entity for the ScheduledMessage-Webhooks.
  */
 @Entity
+@Getter
 @AllArgsConstructor
 @Table(name = "ScheduledMessageWebhooks")
 public class WebhookScheduledMessage extends Webhook {
